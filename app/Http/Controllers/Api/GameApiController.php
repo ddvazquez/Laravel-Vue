3dce26fadc9cc1d0aadf92fbc51f94360a04b86c
davidv <dvazquez@simplifica.es>
2023-02-08 15:19:53 +0100

    Create Controller and API Resource

---
+++ app/Http/Controllers/Api/GameApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Game;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\GameResource;
+
+class GameApiController extends Controller
+{
+    public function index()
+    {
+        return GameResource::collection(Game::all());
+    }
+
+    public function show(Game $game)
+    {
+        return new GameResource($game);
+    }
+
+    public function destroy(Game $game)
+    {
+        $game->delete();
+        return response()->noContent();
+    }
+}
